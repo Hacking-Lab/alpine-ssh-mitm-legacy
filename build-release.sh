3cb5e2df69ac8e80c1e719be2677f898c6594078
Ivan Buetler <ivan.buetler@compass-security.com>
2022-11-08 21:09:44 +0100

    update

--- build-release.sh
+++ build-release.sh
@@ -5,13 +5,13 @@ case `uname -m` in
 	x86_64)
 		echo "=============================================="
 		echo "building x86_64"
-		docker buildx build --platform linux/amd64 --no-cache -t hackinglab/alpine-ssh-mitm:amd64-latest -t hackinglab/alpine-ssh-mitm:amd64-$1 -t hackinglab/alpine-ssh-mitm:amd64-$1.0 -f Dockerfile.amd64 .
+		docker buildx build --platform linux/amd64 --no-cache -t hackinglab/alpine-ssh-mitm-legacy:amd64-latest -t hackinglab/alpine-ssh-mitm-legacy:amd64-$1 -t hackinglab/alpine-ssh-mitm-legacy:amd64-$1.0 -f Dockerfile.amd64 .
 	;;
 
 	aarch64)
                 echo "=============================================="
                 echo "building arm64"
-                docker buildx build --platform linux/arm64 --no-cache -t hackinglab/alpine-ssh-mitm:arm64-latest -t hackinglab/alpine-ssh-mitm:arm64-$1 -t hackinglab/alpine-ssh-mitm:arm64-$1.0 -f Dockerfile.arm64 .
+                docker buildx build --platform linux/arm64 --no-cache -t hackinglab/alpine-ssh-mitm-legacy:arm64-latest -t hackinglab/alpine-ssh-mitm-legacy:arm64-$1 -t hackinglab/alpine-ssh-mitm-legacy:arm64-$1.0 -f Dockerfile.arm64 .
 	;;
 
 	*)
